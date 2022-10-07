fbad8f9854e8ef193281f707ef8e5ba1c287ca71
Doğukan Atakul <datakul@yandex.com>
2022-10-07 18:39:31 +0300

    new version.

--- src/Pdf.php
+++ src/Pdf.php
@@ -1,48 +1,83 @@
-<?php namespace Gufy\PdfToHtml;
+<?php
+
+namespace Gufy\PdfToHtml;
 
 class Pdf
 {
-  protected $file, $info;
-  protected $info_bin = '/usr/bin/pdfinfo';
-  public function __construct($file, $options=array())
-  {
-    $this->file = $file;
-    $class = $this;
-    array_walk($options, function($item, $key) use($class){
-      $class->$key = $item;
-    });
-    $this->info($file);
-    return $this;
-  }
-  public function getInfo()
-  {
-    return $this->info;
-  }
-  protected function info()
-  {
-    // echo $this->info_bin.' '.$this->file;
-    $content = shell_exec($this->info_bin.' '.$this->file);
-    $options = explode("\n", $content);
-    $info = array();
-    foreach($options as &$item)
+    protected $file;
+    protected $info;
+
+    public function __construct($file, $options = [])
     {
-      if(!empty($item))
-      {
-        list($key, $value) = explode(":", $item);
-        $info[str_replace(array(" "),array("_"),strtolower($key))] = trim($value);
-      }
+        $this->file = $file;
+        $class = $this;
+        array_walk($options, function ($item, $key) use ($class) {
+            $class->$key = $item;
+        });
+
+        return $this;
     }
-    // print_r($info);
-    $this->info = $info;
-    return $this;
-    // return $content;
-  }
-  public function html()
-  {
-    return new Html($this->file);
-  }
-  public function getPages()
-  {
-    return $this->info['pages'];
-  }
+
+    public function getInfo()
+    {
+        $this->checkInfo();
+
+        return $this->info;
+    }
+
+    protected function info()
+    {
+
+        if (PHP_OS === 'WINNT') {
+            $content = shell_exec('"'.$this->bin().'" "'.$this->file.'"');
+        }
+        else {
+            $content = shell_exec($this->bin()." '".$this->file."'");
+        }
+
+        // print_r($info);
+        $options = explode("\n", $content);
+        $info = [];
+        foreach ($options as &$item) {
+            if (!empty($item)) {
+                list($key, $value) = explode(":", $item);
+                $info[ str_replace([" "], ["_"], strtolower($key)) ] = trim($value);
+            }
+        }
+        $this->info = $info;
+
+        return $this;
+    }
+
+    public function getDom($options = [])
+    {
+        $this->checkInfo();
+
+        return new Html($this->file, $options);
+    }
+
+    public function html($page = 1, $options = [])
+    {
+        $dom = $this->getDom($options);
+
+        return $dom->raw($page);
+    }
+
+    public function getPages()
+    {
+        $this->checkInfo();
+
+        return $this->info['pages'];
+    }
+
+    private function checkInfo()
+    {
+        if ($this->info == null)
+            $this->info();
+    }
+
+    public function bin()
+    {
+        return Config::get('pdfinfo.bin', '/usr/bin/pdfinfo');
+    }
 }
